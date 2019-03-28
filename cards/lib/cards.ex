9799d770e1700b128c0d6d25d14c581e799181ba
TheBrockstar <00AgentB@gmail.com>
2019-03-27 21:52:56 -0600

    Implement deal(deck,size) using Enum.split()

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,7 +1,7 @@
 defmodule Cards do
 
   # Create a deck by iterating through a list of
-  # suits and a list of values.
+  # suits and a list of values
   def create_deck do
     values = ["Ace","Two","Three","Four","Five"]
     suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
@@ -11,7 +11,7 @@ defmodule Cards do
   end
 
   # Return a deck of cards that matches a shuffled
-  # version of the deck passed in as an argument.
+  # version of the deck passed in as an argument
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
@@ -20,5 +20,11 @@ defmodule Cards do
   # return True it does, False if not
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  # Return a hand of cards of a given size and
+  # a deck of cards missing the cards in the hand
+  def deal(deck, size) do
+    Enum.split(deck, size)
   end
 end
